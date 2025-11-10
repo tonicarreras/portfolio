11d9b0a9dd02abe1dadae4af043cc145e50aedb3
Toni Carreras <antoniocarrerasalarcon@gmail.com>
2025-11-10 20:23:48 +0100

    refactor: remove server allowed hosts from astro.config; enhance Newsletter component with rounded corners; completely redesign SpotifyPlayer component for improved functionality and layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,6 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  },
-  server: {
-    allowedHosts: [
-      'tradition-place-teach-shuttle.trycloudflare.com',
-      '.trycloudflare.com'
-    ]
   }
 
 })
